dbe4f1359f239f674076085fe25f060eb0f7811c
lautaro <gerez.lautaro12@gmail.com>
2024-10-18 17:22:12 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+include_once("configuration/Configuration.php");
+$configuration = new Configuration();
+$router = $configuration->getRouter();
+
+$rout = isset($_GET['action']) ? $_GET['action'] : " ";
+$rout1 = isset($_GET['page']) ? $_GET['page'] : " ";
+
+$router->route($rout1, $rout);
